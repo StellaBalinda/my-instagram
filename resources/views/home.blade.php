2b89896f75e4cbdde32907e3accafbb3913a6b0a
StellaBalinda <41113674+StellaBalinda@users.noreply.github.com>
2023-08-01 13:15:30 +0200

    Adding Username to the Registration,Creating the Profiles Controller, Fetching the Record From The Database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,16 +7,15 @@
             <img src="https://miro.medium.com/v2/resize:fit:2400/1*B6_f-_SxscJ9FCuIjOrQAQ.jpeg" width="80%" height="100%" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>freeCodeCamp</h1></div>
+            <div><h1>{{$user->username}}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153 </strong>posts</div>
                 <div class="pr-5"><strong>23k </strong>followers</div>
                 <div class="pr-5"><strong>212 </strong>following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-            <div>We're a global community of millions of people learning to code together. 
-                LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/ </div>
-            <div><a href="#">www.freeCodeCamp.org</a></div>
+            <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
+            <div>{{$user->profile->description}} </div>
+            <div><a href="#">{{$user->profile->url}}</a></div>
         </div>
     </div>
     <div>
